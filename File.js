b82dcc11647bcc8accdae1f156707f2664db4725
alexisrhae13 <101431243+alexisrhae13@users.noreply.github.com>
2025-03-25 00:40:14 -0400

    Update File.js

--- File.js
+++ File.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Ensure that DOM is fully loaded
+  // Ensure that DOM is fully loaded before attaching event listeners
+
+  // Scroll listener for showing roads
   document.addEventListener("scroll", function () {
     let scrollPosition = window.scrollY;
 
@@ -10,31 +12,36 @@ document.addEventListener("DOMContentLoaded", function () {
       roadContainer.classList.add("show-roads");
     }
   });
-});
-document.querySelector("#about-btn").addEventListener("click", function() {
+
+  // Event listener for About Me anchor
+  document.querySelector("#about-btn").addEventListener("click", function() {
     document.querySelector("#needle-north").style.transform = "rotate(0deg)";
     document.querySelector("#needle-east").style.transform = "rotate(90deg)";
     document.querySelector("#needle-south").style.transform = "rotate(180deg)";
     document.querySelector("#needle-west").style.transform = "rotate(270deg)";
-});
+  });
 
-document.querySelector("#gis-btn").addEventListener("click", function() {
+  // Event listener for GIS anchor
+  document.querySelector("#gis-btn").addEventListener("click", function() {
     document.querySelector("#needle-north").style.transform = "rotate(0deg)";
     document.querySelector("#needle-east").style.transform = "rotate(90deg)";
     document.querySelector("#needle-south").style.transform = "rotate(180deg)";
     document.querySelector("#needle-west").style.transform = "rotate(270deg)";
-});
+  });
 
-document.querySelector("#experience-btn").addEventListener("click", function() {
+  // Event listener for Past Gigs anchor
+  document.querySelector("#experience-btn").addEventListener("click", function() {
     document.querySelector("#needle-north").style.transform = "rotate(180deg)";
     document.querySelector("#needle-east").style.transform = "rotate(270deg)";
     document.querySelector("#needle-south").style.transform = "rotate(0deg)";
     document.querySelector("#needle-west").style.transform = "rotate(90deg)";
-});
+  });
 
-document.querySelector("#contact-btn").addEventListener("click", function() {
+  // Event listener for Contact anchor
+  document.querySelector("#contact-btn").addEventListener("click", function() {
     document.querySelector("#needle-north").style.transform = "rotate(90deg)";
     document.querySelector("#needle-east").style.transform = "rotate(0deg)";
     document.querySelector("#needle-south").style.transform = "rotate(270deg)";
     document.querySelector("#needle-west").style.transform = "rotate(180deg)";
+  });
 });
